0db001ed14bedb21f9d95bf8661dc788e6606d35
andre <andrebertonhadev@gmail.com>
2020-11-05 11:08:32 -0800

    configuracao babel

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frontend
+
+## Added babel dependencies see package.json 
+
+### do yarn install or simple yarn to install libraries
+
++ To run the project:
+    => yarn babel src/index.js --out-file public/bundle.js
---
+++ public/script.js
@@ -0,0 +1,5 @@
+/**
+   * Babel: converter (transpilar) codigo react para um codigo que o browser entenda
+   * Webpack: arquivos js css png , para cada tipo vou converter o codigo de uma maneira diferente
+   * Loaders: babel-loader css-loader image-loader file-loader 
+ */
